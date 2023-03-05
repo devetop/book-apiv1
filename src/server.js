d6d2bb9d798ffcc1a9638c4b334bdd7ec396ef28
Ubuntu <ubuntu@ip-172-26-15-57.ap-southeast-1.compute.internal>
2023-03-05 14:38:23 +0000

    update

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import Hapi from "@hapi/hapi";
-import routes from "./src/routes.js";
+import routes from "./routes.js";
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: 9000,
+    host: "0.0.0.0",
     routes: {
       cors: {
         origin: ["*"],
